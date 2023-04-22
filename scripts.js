d4988c23f77c27d82ce5eaa9b0e4a5c6caf5e82f
Andrew Virts <96750426+asvirts@users.noreply.github.com>
2023-04-22 16:19:26 -0500

    font sizes

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ async function fact() {
     const data = await fetch("https://uselessfacts.jsph.pl/api/v2/facts/random")
     const fact = await data.json()
     console.log(fact)
-    document.getElementById("test").innerHTML = fact.text
+    document.getElementById("fact").innerHTML = fact.text
   }
   
   fact()
