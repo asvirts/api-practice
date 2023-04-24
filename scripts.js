e10e04f5e1fe060162498b3e6ead46c7a9240de8
Andrew Virts <96750426+asvirts@users.noreply.github.com>
2023-04-24 16:54:44 -0500

    huh?

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 async function fact() {
-    const data = await fetch("https://uselessfacts.jsph.pl/api/v2/facts/random")
+    const data = await fetch("mongodb+srv://asvirts:Nashville4117@cluster0.udils2m.mongodb.net/test")
     const fact = await data.json()
     console.log(fact)
     document.getElementById("fact").innerHTML = fact.text
