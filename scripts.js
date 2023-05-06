26954bc758d2a11d5b90dcbf53ee4518e8a380d8
Andrew Virts <asvirts@outlook.com>
2023-05-06 14:38:19 -0500

    practice makes perfect

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
-async function fact() {
+async function items() {
     const data = await fetch("https://api.nytimes.com/svc/books/v3/lists/names.json?api-key=KKycLqaAQfhTDGPrYQXnT2Mcm71dUENk")
     const books = await data.json()
-    const el = document.getElementById("fact")
+    const el = document.getElementById("items")
     el.textContent = "List:"
     el.appendChild(document.createElement("li"))
 
   }
   
-  fact()
+  items()
